b13d7b6add0422ebbd3d22e9a0cf8161c24d188f
Aditya Soni <adityas@geekyants.com>
2019-10-23 13:58:23 +0530

    added player spaceship image logo

--- main.py
+++ main.py
@@ -4,20 +4,35 @@ import pygame
 
 pygame.init()
 
-# creating screen of height 800 and width 600
+# creating screen of width 800 and height 600
 screen = pygame.display.set_mode((800, 600))
 
 # Title and logo
 pygame.display.set_caption("Space war")
 icon = pygame.image.load('./assets/project_logo.png')
 pygame.display.set_icon(icon)
+
+# player
+playerImg = pygame.image.load('./assets/spaceship.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # game loop
 running = True
 while running:
+    # filling rgb color on the screen
+    screen.fill((0, 0, 0))
+    # capturing events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # filling rgb color on the screen
-    screen.fill((0, 0, 0))
+
+    # drawing the player
+    player()
     # updating the window(state)
     pygame.display.update()
